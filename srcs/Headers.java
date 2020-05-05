c6013fee40e817c03508268aa2ad7904d06de3f3
miktim <miktim@mail.ru>
2020-05-05 11:10:54 +0300

    Android compatibility

---
+++ srcs/Headers.java
@@ -0,0 +1,32 @@
+/*
+ * Headers, MIT (c) 2020 miktim@mail.ru
+ *
+ * Replaces the core class com.sun.net.httpserver.Headers
+ * Added for Android compatibility
+ */
+package org.samples.java.websocket;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Headers {
+
+    private final HashMap<String, String> headers = new HashMap<String, String>();
+
+    public String getFirst(String key) {
+        return headers.get(key);
+    }
+
+    public void add(String key, String value) {
+        String val = getFirst(key);
+        if (val == null || val.isEmpty()) {
+            headers.put(key, value);
+        } else {
+            headers.put(key, val + "," + value);
+        }
+    }
+    
+    public Set<String> keySet() {
+        return headers.keySet();
+    }
+}
