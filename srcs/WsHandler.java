93b7ce08de9d791383d65290cc5238e451405453
miktim <miktim@mail.ru>
2020-10-10 14:25:56 +0300

    2.0.0

--- srcs/WsHandler.java
+++ srcs/WsHandler.java
@@ -1,5 +1,5 @@
 /*
- * WsHandler. WebSocket  handler, MIT (c) 2020 miktim@mail.ru
+ * WsHandler. WebSocket handler, MIT (c) 2020 miktim@mail.ru
  *
  * Created: 2020-03-09
  */
@@ -10,7 +10,10 @@ public interface WsHandler {
     public void onClose(WsConnection conn);
     public void onMessage(WsConnection conn, String s );
     public void onMessage(WsConnection conn, byte[] b);
-// onError: conn may be null in the server handler (fatal ServerSocket exception)
+// onError: conn may be null in the listener handler (fatal ServerSocket exception)
 //          check connection closure status
     public void onError(WsConnection conn, Exception e);
+// for the future if the maximum message length is exceeded
+//   public void onStream(WsConnection conn, WsConnection.TextInputStream is );
+//   public void onStream(WsConnection conn, WsConnection.BinaryInputStream is);
 }
